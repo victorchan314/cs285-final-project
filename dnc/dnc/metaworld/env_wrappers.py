6fbeb0e63cd8a491dd0efb70b1354822fb1363c2
Victor Chan <victorchan88888888@gmail.com>
2019-12-14 16:31:58 -0800

    Get and Set param values functions

--- dnc/dnc/metaworld/env_wrappers.py
+++ dnc/dnc/metaworld/env_wrappers.py
@@ -2,10 +2,11 @@ import gym
 from metaworld.benchmarks import ML10
 from rllab.envs.env_spec import EnvSpec
 from rllab.envs.gym_env import convert_gym_space
+from rllab.envs.proxy_env import ProxyEnv
 
 class ML10Wrapper(gym.Env):
     def __init__(self, wrapped_env):
-        self.env = wrapped_env
+        self._wrapped_env = wrapped_env
         observation_space = wrapped_env.observation_space
         action_space = wrapped_env.action_space
         self.observation_space = convert_gym_space(observation_space)
@@ -13,16 +14,22 @@ class ML10Wrapper(gym.Env):
         #self.env_spec = EnvSpec(self.observation_space, self.action_space)
 
     def step(self, action):
-        return self.env.step(action)
+        return self._wrapped_env.step(action)
 
     def reset(self):
-        return self.env.reset()
+        return self._wrapped_env.reset()
 
     def render(self, mode="human"):
-        return self.env.render(mode)
+        return self._wrapped_env.render(mode)
 
     def close(self):
-        return self.env.close()
+        return self._wrapped_env.close()
 
     def seed(self, seed=None):
-        return self.env.seed(seed)
+        return self._wrapped_env.seed(seed)
+
+    def get_param_values(self):
+        return None
+
+    def set_param_values(self, params):
+        pass
