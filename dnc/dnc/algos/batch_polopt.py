ac63c692a1d7959a03dd1c5dd3511579f64be790
Victor Chan <victorchan88888888@gmail.com>
2019-12-18 22:39:48 -0800

    Revert weird git changes

--- dnc/dnc/algos/batch_polopt.py
+++ dnc/dnc/algos/batch_polopt.py
@@ -256,16 +256,6 @@ class BatchPolopt(RLAlgorithm):
                 success_rate = successes / trials
                 logger.record_tabular("SuccessRate", success_rate)
 
-                successes = 0.0
-                trials = 0.0
-                for samples_data in all_samples_data:
-                    success = samples_data["env_infos"]["success"]
-                    successes += np.mean(success)
-                    trials += success.shape[0]
-
-                success_rate = successes / trials
-                logger.record_tabular("Success Rate", success_rate)
-
                 logger.log("Saving snapshot...")
                 params = self.get_itr_snapshot(itr, all_samples_data)  # , **kwargs)
                 logger.save_itr_params(itr, params)
